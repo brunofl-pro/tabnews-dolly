3276124e37a94be45d36bc13a40fdce50d8a7fab
brunofl-pro <139413357+brunofl-pro@users.noreply.github.com>
2023-07-13 01:50:56 +0000

    dani msg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi Isis, se ta vendo esta mensagem, dá uma risadinha :D</h1>
+    return (<div><h1>Oi Dani,</h1> <h1>te amo linda</h1> <h3>...se você está vendo esta mensagem, dá uma risadinha :D</h3></div>)
 
 }
 
