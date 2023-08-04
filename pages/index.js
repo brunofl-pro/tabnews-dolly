84dca891b06ba5cccf7b2dd0b4c9e4eca23a7c91
brunofl-pro <139413357+brunofl-pro@users.noreply.github.com>
2023-08-04 03:05:45 +0000

    #fix we are under construction

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return (<div><h1>This is pyrobits</h1> <h1>a naive community for game sketches</h1> <h3>...If you think developping game ideas is funnier than playing them, you are in the right place.</h3></div><div><p>We are under construction</p></div>)
+    return (<div><h1>This is pyrobits</h1> <h1>a naive community for game sketches</h1> <h3>...If you think developping game ideas is funnier than playing them, you are in the right place.</h3><p>We are under construction</p></div>)
 
 }
 
